1366758ed8017c6ae14d1cfde3920bff544020ca
jooooock <findsource@proton.me>
2024-04-02 10:19:09 +0800

    update

--- src/session.js
+++ src/session.js
@@ -1,11 +1,22 @@
+/*========================== 可自定义此账户信息 =================================*/
+const profile = {
+    name: 'Jack',
+    email: 'findsource@proton.me',
+    avatar: 'https://avatars.githubusercontent.com/u/156505552?v=4',
+}
+/*============================================================================*/
+
+
 window.localStorage.setItem("sb-bxkgqurwqjmvrqekcbws-auth-token", JSON.stringify({
     access_token: "",
     refresh_token: "",
     expires_at: 0,
     user: {
         user_metadata: {
-            name: 'Jock'
+            name: profile.name,
+            avatar_url: profile.avatar,
         },
-        email: 'findsource@proton.me'
-    }
+        email: profile.email
+    },
+    __from__: "jsoncracker"
 }))
